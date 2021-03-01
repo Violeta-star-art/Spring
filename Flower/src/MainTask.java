418580fdec751dc2f17aba65c2a05edf8d491920
Violeta-star-art <violetateova@gmail.com>
2021-03-02 00:06:58 +0100

    amend comit

--- Flower/src/MainTask.java
+++ Flower/src/MainTask.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class MainTask {
+public class MainTask {
+        public static void main(String[] args) {
+
+            System.out.println("SMile to Me");
+            System.out.println("Hello Sun");
+
+        }
 }
